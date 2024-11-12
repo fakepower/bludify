23ac9c55100158541da30dfc026ab298735778ee
fakepower <138264321+fakepower@users.noreply.github.com>
2024-11-11 21:36:23 -0300

    Update bludify.py

--- bludify.py
+++ bludify.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 st.title("bludify.com!!!!")
@@ -10,13 +11,16 @@ m2 = st.button("KABOOM!")
 m3 = st.button("blue lobster")
 
 if m1:
-    audio_path = "https://drive.google.com/uc?export=download&id=1pYetLCMcfWIX39QbY6zD81A7KbZvahab"
-    st.audio(audio_path)
+    audio_url = "https://drive.google.com/uc?export=download&id=1pYetLCMcfWIX39QbY6zD81A7KbZvahab"
+    audio_html = f'<audio controls><source src="{audio_url}" type="audio/mpeg"></audio>'
+    st.markdown(audio_html, unsafe_allow_html=True)
 
 if m2:
-    audio_path = "https://drive.google.com/uc?export=download&id=19WydQkhrIG7rcmXEuuBWrU8oOv1yMJjg"
-    st.audio(audio_path)
+    audio_url = "https://drive.google.com/uc?export=download&id=19WydQkhrIG7rcmXEuuBWrU8oOv1yMJjg"
+    audio_html = f'<audio controls><source src="{audio_url}" type="audio/mpeg"></audio>'
+    st.markdown(audio_html, unsafe_allow_html=True)
 
 if m3:
-    audio_path = "https://drive.google.com/uc?export=download&id=17cVDPjhkxLdU1mDwrJROXAend-J-ViO-"
-    st.audio(audio_path)
+    audio_url = "https://drive.google.com/uc?export=download&id=17cVDPjhkxLdU1mDwrJROXAend-J-ViO-"
+    audio_html = f'<audio controls><source src="{audio_url}" type="audio/mpeg"></audio>'
+    st.markdown(audio_html, unsafe_allow_html=True)
