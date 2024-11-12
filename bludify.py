da5e819ed351e61b1d176c37035f697d3ceb65ea
fakepower <138264321+fakepower@users.noreply.github.com>
2024-11-11 21:34:36 -0300

    Update bludify.py

--- bludify.py
+++ bludify.py
@@ -1,18 +1,22 @@
 import streamlit as st
+
 st.title("bludify.com!!!!")
 st.header("from bluds, to bluds")
 st.subheader("the best music app for bluds")
+
 # Caminho do arquivo com string raw
 m1 = st.button("what a sigma")
 m2 = st.button("KABOOM!")
 m3 = st.button("blue lobster")
-if m1:
-    audio_path = r"https://drive.google.com/uc?export=download&id=1pYetLCMcfWIX39QbY6zD81A7KbZvahab"
 
+if m1:
+    audio_path = "https://drive.google.com/uc?export=download&id=1pYetLCMcfWIX39QbY6zD81A7KbZvahab"
     st.audio(audio_path)
+
 if m2:
-    audio_path = r"https://drive.google.com/uc?export=download&id=19WydQkhrIG7rcmXEuuBWrU8oOv1yMJjg"
+    audio_path = "https://drive.google.com/uc?export=download&id=19WydQkhrIG7rcmXEuuBWrU8oOv1yMJjg"
     st.audio(audio_path)
+
 if m3:
-    audio_path = r"https://drive.google.com/uc?export=download&id=17cVDPjhkxLdU1mDwrJROXAend-J-ViO-"
+    audio_path = "https://drive.google.com/uc?export=download&id=17cVDPjhkxLdU1mDwrJROXAend-J-ViO-"
     st.audio(audio_path)
